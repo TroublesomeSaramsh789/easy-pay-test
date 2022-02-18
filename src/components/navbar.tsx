228c28a8117c36fac7f749753ac50ada094b0056
troublesome <killer789b@gmail.com>
2022-02-18 13:27:03 +0545

    jsx to tsx

---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import { Flex, Text, Input, Spacer } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Flex color="gray.50" bg="gray.800" py="0.5rem" px="1rem" w="100%" jus>
+      <Text mx="1rem" fontSize="xl">
+        EasyPAY
+      </Text>
+      <Spacer />
+
+      <Input
+        color="gray.50"
+        bg="gray.600"
+        borderRadius="0.3rem"
+        maxW="300px"
+        size="sm"
+        type="text"
+        placeholder="Search for anything"
+      />
+      <Spacer />
+      <Text mx="1rem" fontSize="md">
+        Login
+      </Text>
+      <Text mx="1rem" fontSize="md">
+        Register
+      </Text>
+    </Flex>
+  );
+};
+
+export default Navbar;
